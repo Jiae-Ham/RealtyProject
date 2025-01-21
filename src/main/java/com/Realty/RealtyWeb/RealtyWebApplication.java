5b0ce1a664f13b02ca30c325e425672f88da7f7f
Jiae Ham <coup31307@koreatech.ac.kr>
2025-01-21 23:46:17 +0900

    Spring-boot Setting
    Setting

---
+++ src/main/java/com/Realty/RealtyWeb/RealtyWebApplication.java
@@ -0,0 +1,13 @@
+package com.Realty.RealtyWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Realty/RealtyWeb/RealtyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Realty.RealtyWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
